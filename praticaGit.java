943c00907ec5f0fb2f5e62f028ee1c1bcd805733
thiago1 <122583817+thiagobertodel@users.noreply.github.com>
2026-08-25 17:32:04 -0300

    Add files via upload

--- praticaGit.java
+++ praticaGit.java
@@ -1,5 +1,41 @@
-public class praticaGit{
-public class static void main (String[]args){
-	System.out.println("Farina chato");
-	}
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class praticaGit {
+
+    // a. Método que retorna quantas ocorrências de um elemento estão na lista
+    public static int nrOcorrencias(ArrayList<Integer> l, Integer el) {
+        if (l == null || el == null) return 0;
+        int count = 0;
+        for (Integer num : l) {
+            if (el.equals(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    
+
+
+
+    // d. Método que retorna uma lista de elementos repetidos de l
+    public static ArrayList<Integer> listRepeat(ArrayList<Integer> l) {
+        ArrayList<Integer> repetidos = new ArrayList<>();
+        if (l == null) return repetidos;
+
+        Set<Integer> vistos = new HashSet<>();
+        Set<Integer> adicionados = new HashSet<>();
+
+        for (Integer num : l) {
+            if (!vistos.add(num) && adicionados.add(num)) {
+                repetidos.add(num);
+            }
+        }
+        return repetidos;
+    }
+
+
+
 }
